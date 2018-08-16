5ebaf9c4a2bf300ef3890e524a7d26a61ad8a075
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-15 22:21:51 -0500

    Atom Update

---
+++ ZedShaw/09_Printing_Printing_Printing.py
@@ -0,0 +1,11 @@
+# Python - The Hard Way - Exercise 9 - Printing, Printing, Printing
+
+# Here's some new strange stuff, remember to type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+# \n starts a new line for a string
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
