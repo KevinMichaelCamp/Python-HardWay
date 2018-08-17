239c3024729b52baca05a8e64afe80ad5875d896
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-16 22:20:27 -0500

    Python

---
+++ ZedShaw/13_Parameters_Unpacking_Variables.py
@@ -0,0 +1,13 @@
+# Python - The Hard Way - Exercise 13 - Parameters, Unpacking, Variables
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+fourth = raw_input("Enter fourth variable... "),
+print fourth
