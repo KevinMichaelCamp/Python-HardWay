eb3680867a2ef7447b4029178841d26531fb3936
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-19 22:07:22 -0500

    Reading Files

--- ZedShaw/15_Reading_Files.py
+++ ZedShaw/15_Reading_Files.py
@@ -1,1 +1,18 @@
 # Python - The Hard Way - Exercise 14 - Reading Files
+
+# import argv (Argument Variable) from sys module (system)
+from sys import argv
+# Argument Variables(2) = script(scriptname) & filename(.txt input file)
+script, filename = argv
+# Open file (filename) and store in variable (txt)
+txt = open(filename)
+# Read file (txt)
+print "Here's your file %r:" % filename
+print txt.read()
+# Use raw_input to input filename (txt)
+print "Type the filename again:"
+file_again = raw_input("~ ")
+# Open file
+txt_again = open(file_again)
+# Read file
+print txt_again.read()
