3bbd32620fc8277da7a0c404373deedf069d4afe
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-19 21:19:08 -0500

    Python 13

--- ZedShaw/13_Parameters_Unpacking_Variables.py
+++ ZedShaw/13_Parameters_Unpacking_Variables.py
@@ -8,6 +8,8 @@ print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
+#print length of argv
+print "Number of arguments", len(argv)
 
-fourth = raw_input("Enter fourth variable... "),
+fourth = raw_input("Enter fourth variable... ")
 print fourth
