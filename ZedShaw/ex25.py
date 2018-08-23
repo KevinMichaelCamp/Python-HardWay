9b45d5085b5e149253a6b718641b26f573e68fc3
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-08-22 21:43:59 -0500

    Python THW - Finished part 1!!!

---
+++ ZedShaw/ex25.py
@@ -0,0 +1,43 @@
+# Python - The Hard Way - Exercise 25 - Even More Practice
+
+# split method
+def break_words(stuff):
+    """This function will break up words for us."""
+    words =  stuff.split(' ')
+    return words
+
+# sorted method
+def sort_words(words):
+    """Sorts the words"""
+    return sorted(words)
+
+# pop method
+def print_first_word(words):
+    """Prints the first word after popping it off."""
+    word = words.pop(0)
+    print word
+
+# pop method with last index (len - 1)
+def print_last_word(words):
+    """Prints the last word after popping it off."""
+    word = words.pop(-1)
+    print word
+
+# break_words method
+def sort_sentence(sentence):
+    """Takes in a full sentence and returns the sorted words."""
+    words = break_words(sentence)
+    return sort_words(words)
+
+# function using above functions
+def print_first_and_last(sentence):
+    """Prints the first and last words of a sentence."""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+    """Sorts the words then prints the first and last one."""
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
