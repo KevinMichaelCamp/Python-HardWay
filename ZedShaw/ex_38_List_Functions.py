f9074b4725a4691e76b79a0a4015f0fc3d1dfc32
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-10-19 16:27:01 -0500

    ex_38_List_Functions

---
+++ ZedShaw/ex_38_List_Functions.py
@@ -0,0 +1,5 @@
+# Python - The Hard Way - Exercise 38 - List Functions
+
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+print("Wait, there's not 10 things in that list, let's fix that.")
