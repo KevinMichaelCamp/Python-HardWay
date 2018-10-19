9b66e136174162ea77126d023ed563ef4762e426
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-10-19 15:06:44 -0500

    ex_32_Loops_and_Lists

---
+++ ZedShaw/ex_32_Loops_and_Lists.py
@@ -0,0 +1,7 @@
+# Python - The Hard Way - Exercise 32 - Loops & Lists
+
+count = [1,2,3,4,5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+#this first kind of for-loop goes through a list
