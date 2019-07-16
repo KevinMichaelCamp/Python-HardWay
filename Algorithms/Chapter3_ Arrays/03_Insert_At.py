914a69b5665e0b46eca000db9b90462d7fd7b4b1
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2019-07-15 20:13:37 -0500

    Algorithms

---
+++ Algorithms/Chapter3_ Arrays/03_Insert_At.py
@@ -0,0 +1,8 @@
+# Given an array, an index, and an additional value, insert the value into the array at the given index. No built-ins e.g.(.insert(index, value))
+
+def insertAt(arr, index, value):
+    arr[:] = arr[:index] + [value] + arr[index:]
+    return arr
+
+# Test Cases
+print(insertAt([1,2,3,5,6], 3, 4))
