72e3ee24c8db82651bd7686fda1fa4d93ab05c29
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2019-07-26 07:29:28 -0500

    Algorithms

--- Algorithms/Chapter4_Strings/05_Remove_Shorter_Strings.py
+++ Algorithms/Chapter4_Strings/05_Remove_Shorter_Strings.py
@@ -1,8 +1,10 @@
 # Given a string array and value(length), remove any strings shorter than length from the array.
 
 def removeShorterStrings(array, length):
-
-
+    for string in array:
+        if len(string) < length:
+            array.remove(string)
+    return array
 
 
 # Test Cases
